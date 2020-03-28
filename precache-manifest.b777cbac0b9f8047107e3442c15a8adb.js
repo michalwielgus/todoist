8b6e48386a70246a93626d0531d3523942b470c4
Michal Mieculewicz-Wielgus <wielgus.michaal@gmail.com>
2020-03-28 15:48:16 +0100

    Updates

---
+++ precache-manifest.b777cbac0b9f8047107e3442c15a8adb.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fac2e74ca03910b230752b758fd919f1",
+    "url": "/todoist/index.html"
+  },
+  {
+    "revision": "e342bb0224230fb9a3d3",
+    "url": "/todoist/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "655df27a632e3d83d077",
+    "url": "/todoist/static/js/2.937873c0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/todoist/static/js/2.937873c0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e342bb0224230fb9a3d3",
+    "url": "/todoist/static/js/main.06a869e5.chunk.js"
+  },
+  {
+    "revision": "4da31f555939d5549864",
+    "url": "/todoist/static/js/runtime-main.24f8fcb2.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/todoist/static/media/logo.5d5d9eef.svg"
+  }
+]);
